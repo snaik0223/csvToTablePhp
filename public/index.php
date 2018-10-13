6de86f03825f9ea090785c7980989051e293f95c
Shamix <nsammi11@gmail.com>
2018-10-13 06:49:19 -0400

    first commit

---
+++ public/index.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Shamix
+ * Date: 10/13/18
+ * Time: 6:39 AM
+ */
+
+echo 'Hello welcome to my project!';
---
+++ readme.md
@@ -0,0 +1,2 @@
+# CSV Project
+1. Hi this is the readme file for my php code for generating a Bootstrap table
