e922032c6eaec51ff186980d46c892f72b8d55b6
Shamix <nsammi11@gmail.com>
2018-10-13 07:52:12 -0400

    testing

--- public/index.php
+++ public/index.php
@@ -7,3 +7,37 @@
  */
 
 echo 'Hello welcome to my project!';
+
+
+class csvReader{
+    private $filename;
+
+    function getFilename(){
+        return($this->filename);
+    }
+
+    function __construct($value){
+        $this->filename = $value;
+    }
+
+
+    public static function dataArray(){
+        $data = array();
+
+        $file = fopen('sample.csv','r');
+
+        $counter = 0;
+        while (($line = fgetcsv($file)) !== FALSE) {
+            //do nothing
+            $data[] = $line;
+            $counter++;
+            echo $counter.'<br>';
+        }
+        fclose($file);
+
+    }
+
+}
+
+$readerObject = new csvReader('sample.csv');
+csvReader::dataArray();
